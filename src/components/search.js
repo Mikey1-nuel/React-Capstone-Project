2c663d17cb250c728cb91f6460f1374d038c8503
Emmanuel <emmanuelnwoye12@gmail.com>
2023-06-01 09:24:06 +0100

    Update js files

---
+++ src/components/search.js
@@ -0,0 +1,86 @@
+import { useEffect, useState } from 'react';
+import { useParams } from 'react-router';
+import { getCovidDataByCountry } from '../redux/country/countrySlice';
+import Header from './nav';
+
+const DetailsPage = () => {
+  const { name } = useParams();
+  const [state, setState] = useState(null);
+  useEffect(() => {
+    const getDetails = async () => {
+      const countryDetails = await getCovidDataByCountry(name);
+      setState(countryDetails);
+    };
+    getDetails();
+  }, [name]);
+
+  return (
+    <div>
+      {
+      state ? (
+        <div className="detail-container">
+          <Header />
+          <div className="detail-container2">
+            <img src={state.countryInfo.flag} alt="country flag" />
+            <div className="detail-container3">
+              <h2 className="name">
+                <span>Country:&nbsp;&nbsp; </span>
+                {state.country}
+              </h2>
+              <h2 className="name">
+                <span>Continent:&nbsp;&nbsp; </span>
+                {state.continent.toLocaleString()}
+              </h2>
+              <h2 className="name">
+                <span>Population:&nbsp;&nbsp; </span>
+                {state.population.toLocaleString()}
+              </h2>
+              <h2 className="name">
+                <span>Cases:&nbsp;&nbsp; </span>
+                {state.cases.toLocaleString()}
+              </h2>
+              <h2 className="name">
+                <span>Today&apos;s Cases:&nbsp;&nbsp; </span>
+                {state.todayCases.toLocaleString()}
+              </h2>
+              <h2 className="name">
+                <span>Deaths:&nbsp;&nbsp; </span>
+                {state.deaths.toLocaleString()}
+              </h2>
+              <h2 className="name">
+                <span>Today&apos;s Deaths:&nbsp;&nbsp; </span>
+                {state.todayDeaths.toLocaleString()}
+              </h2>
+              <h2 className="name">
+                <span>Recovered:&nbsp;&nbsp; </span>
+                {state.recovered.toLocaleString()}
+              </h2>
+              <h2 className="name">
+                <span>Active:&nbsp;&nbsp; </span>
+                {state.active.toLocaleString()}
+              </h2>
+              <h2 className="name">
+                <span>Tests:&nbsp;&nbsp; </span>
+                {state.tests.toLocaleString()}
+              </h2>
+              <h2 className="name">
+                <span>Cases Per One Million:&nbsp;&nbsp; </span>
+                {state.casesPerOneMillion.toLocaleString()}
+              </h2>
+              <h2 className="name">
+                <span>Deaths Per One Million:&nbsp;&nbsp; </span>
+                {state.deathsPerOneMillion.toLocaleString()}
+              </h2>
+              <h2>{}</h2>
+            </div>
+          </div>
+        </div>
+      ) : (
+        <div>Loading</div>
+      )
+    }
+    </div>
+  );
+};
+
+export default DetailsPage;
--- src/redux/store.js
+++ src/redux/store.js
@@ -1,7 +1,11 @@
 import { configureStore } from '@reduxjs/toolkit';
+import continentReducer, { getContinent } from './country/countrySlice';
 
 const store = configureStore({
-  reducer: {},
+  reducer: {
+    continent: continentReducer,
+  },
 });
+store.dispatch(getContinent());
 
 export default store;
